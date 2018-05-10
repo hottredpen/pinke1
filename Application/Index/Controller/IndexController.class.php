26761e4a6cbf23155d1b5f68a39c9334efe738c3
50212025 <hottredpen@126.com>
2018-05-10 12:35:09 +0800

    d

--- Application/Index/Controller/IndexController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Index\Controller;
-// use Think\Controller;
-use Common\Controller\CommonBaseController;
-class IndexController extends CommonBaseController {
-    public function index(){
-    	echo "hello world";
-    }
-}
